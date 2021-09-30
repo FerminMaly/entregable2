42d2c2ed7f94ab1b097fb8039547a9a27609fac7
Fermin Maly <ferminmalyvarni@gmail.com>
2021-09-30 20:20:05 -0300

    Tamaño canvas redefinido

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ let matrizTablero = [];
 let fichasInicial = 40;
 let fichasJugador1 = fichasInicial;
 let fichasJugador2 = fichasInicial;
+let jugador1 = null;
+let jugador2 = null;
 
 function dibujarTablero(){
     let iteradorY = 0;
-        
+        //465
     for(let y=0; y<384; y+=48){
         let iteradorX = 0;
-        for(let x=0;x<500;x+=50){
+        for(let x=420;x<920;x+=50){
             let image = new Image();
             image.src = "../img/fichero.png";
             let json = {
@@ -32,19 +34,41 @@ function dibujarTablero(){
     }
 }
 
-function dibujarFicha(color, ubicacion){
-    for(let i = 0; i<fichasInicial; i++){
-        let ficha = new Image();
-        ficha.src = "../img/"+color;
-        ubicacion.appendChild(ficha);
+function dibujarFicha(color, jugador, x, y){
+    let ficha = new Image();
+    ficha.src = "../img/"+color;
+    console.log(jugador)
+    if(jugador == jugador1){
+        if(fichasJugador1 > 0){
+            ficha.onload = function(){        
+                ctx.drawImage(ficha, x, y);
+            }
+        }
+    } else if (jugador == jugador2){
+        if(fichasJugador2 > 0){
+            image.onload = function(){        
+                ctx.drawImage(image, x, y);
+            }
+        }
     }
+    
 }
 
 
 function iniciarJS(){
     dibujarTablero();
-    dibujarFicha("fichaAmarilla.png", amarilla);
-    dibujarFicha("fichaRojo.png", roja);
+    dibujarFicha("fichaAmarilla.png", jugador1, 190, 160);
+    dibujarFicha("fichaRojo.png", jugador2, 1111 , 160);
 }
 
 iniciarJS();
+
+function onMouseMove(e){
+ 
+    ctx.moveTo(e.layerX, e.layerY);
+    
+}
+
+canvas.addEventListener('mousedown',onMouseDown, false);
+canvas.addEventListener('mousemove', onMouseMove, false);
+canvas.addEventListener('mouseup', onMouseUp, false);
