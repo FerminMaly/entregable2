93f6c5b3cb8d65f0791d9a608743f8e1aef26dd5
Agustina Bedrossian <agusbedro@gmail.com>
2021-09-29 17:42:08 -0700

    Tablero dibujado

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let matrizTablero =[];
+let matrizTablero = [];
 
 function dibujarTablero(){
-    // | | | | |
     let iteradorY = 0;
-    let iteradorX = 0;
-    for(let y=0; y<canvas.height; y+48){
-        let arr = [];
-        for(let x=0;x<canvas.width;x+50){
-            
+        
+    for(let y=0; y<384; y+=48){
+        let iteradorX = 0;
+        for(let x=0;x<500;x+=50){
+            let image = new Image();
+            image.src = "../fichero.png";
+            let json = {
+                "imagen" : image,
+                "x": x, 
+                "y": y,
+                "iteradorX": iteradorX,
+                "iteradorY": iteradorY
+            };
+            image.onload = function(){        
+                matrizTablero.push(json);
+                ctx.drawImage(image, x, y);
+            }
+            iteradorX++;
         }
+        iteradorY++;
     }
 }
-
-//dibujarTablero();
-let fichero = new Image();
-fichero.src = "fichero.png";
-console.log(fichero.width);
+dibujarTablero();
